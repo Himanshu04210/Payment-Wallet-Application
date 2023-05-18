d9465b29160d6c590f37124f712f55cffaf281e9
Himanshu04210 <himanshupoly76075@gmail.com>
2023-05-18 22:34:14 +0530

    working on entity classes

--- Payment_Wallet_Application/src/main/java/com/masai/Model/Wallet.java
+++ Payment_Wallet_Application/src/main/java/com/masai/Model/Wallet.java
@@ -1,5 +1,14 @@
 package com.masai.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Wallet {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int walletId;
 
 }
